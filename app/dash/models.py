99127ac36006bf2465fe1b9a8c7e93fbbc48e187
naomixox <walu9963@gmail.com>
2025-11-17 02:57:37 +0200

    update dashbaord models

--- app/dash/models.py
+++ app/dash/models.py
@@ -12,18 +12,25 @@ compulsarysubject_class = db.Table(
 
 class CompulsarySubject(db.Model):
     id = db.Column(db.Integer,primary_key=True)
-    classroom_name = db.Column(db.String(50),nullable=False,unique=True) 
+    subject_name = db.Column(db.String(50),nullable=False,unique=True) 
+    subject_code = db.Column(db.String(50),nullable=False,unique=True) 
+
 
 
 
 class OptionalSubject(db.Model):
     id = db.Column(db.Integer,primary_key=True)
-    classroom_name = db.Column(db.String(50),nullable=False,unique=True) 
+    subject_name = db.Column(db.String(50),nullable=False,unique=True) 
+    subject_code = db.Column(db.String(50),nullable=False,unique=True) 
+
+    # fk
+    classroom_id = db.Column(db.Integer,db.ForeignKey('classroom.id'))
+    classroom = db.relationship("Classroom", backref="optional_subjects")
 
 
 teacher_school_record_class = db.Table(
     "teacher_school_record_class",
-    db.Column(db.Integer,db.ForeignKey('seacherschoolrecord.id')),
+    db.Column(db.Integer,db.ForeignKey('teacherschoolrecord.id')),
     db.Column(db.Integer,db.ForeignKey('classroom.id')),
 )
 
@@ -32,10 +39,11 @@ class Classroom(db.Model):
     id = db.Column(db.Integer,primary_key=True) 
     classroom_name = db.Column(db.String(50),nullable=False,unique=True) 
     # relationships
-    compulsary_subjects = db.relationship("CompulsarySubject",secondary=teacher_school_record_class,backref="classroom",lazy="joined")
+    compulsary_subjects = db.relationship("CompulsarySubject",secondary=compulsarysubject_class,backref="classrooms",lazy="joined")
     optional_subjects = db.relationship("OptionalSubject",backref="classroom",uselist=True,lazy="joined")
-    teacher_school_record = db.relationship("TeacherSchoolRecord",secondary=teacher_school_record_class,backref="classroom",lazy="joined")
+    teacher_school_record = db.relationship("TeacherSchoolRecord",secondary=teacher_school_record_class,backref="classrooms",lazy="joined")
     student_school_record = db.relationship("StudentSchoolRecord",backref="classroom",uselist=True,lazy="joined")
+    assignments = db.relationship("ClassAssignments",backref="classroom",uselist=True,lazy="joined")
     # teachers = db.relationship("Teacher",secondary=teacher_class,backref="classroom",lazy="joined")
     # admin = db.relationship("Admin",backref="classroom",uselist=True,lazy="joined")
     # students = db.relationship("Student",backref="classroom",uselist=True,lazy="joined")
@@ -49,15 +57,14 @@ class StudentAttendance(db.Model):
     # fk
     student_id = db.Column(db.Integer,db.ForeignKey('student.id'))
     # relationships
-    student = db.relationship("Student",backref="student_attendance",uselist=False,lazy="joined")
+    student = db.relationship("Student",backref="student_attendances",uselist=False,lazy="joined")
 
 
 class StudentGrade(db.Model):
     id = db.Column(db.Integer,primary_key=True) 
-    exam_name = db.Column(db.String(50),nullable=False,unique=True)
-    exam_code = db.Column(db.String(50),nullable=False,unique=True)
-    exam_subject_Name = db.Column(db.String(50),nullable=False,unique=True)
-    classroom_name = db.Column(db.String(50),nullable=False,unique=True)
+    exam_name = db.Column(db.String(50),nullable=False)
+    exam_code = db.Column(db.String(50),nullable=False)
+    exam_subject_Name = db.Column(db.String(50),nullable=False)
     student_score = db.Column(db.Integer,nullable=False) 
     student_grade = db.Column(db.String(5),nullable=False) 
     created_at = db.Column(db.DateTime,default=datetime.utcnow)
@@ -70,16 +77,15 @@ class StudentGrade(db.Model):
 
 class ClassAssignments(db.Model):
     id = db.Column(db.Integer,primary_key=True) 
-    assignment_name = db.Column(db.String(50),nullable=False,unique=True)
-    assignment_code = db.Column(db.String(50),nullable=False,unique=True)
-    assignment_subject_Name = db.Column(db.String(50),nullable=False,unique=True)
-    classroom_name = db.Column(db.String(50),nullable=False,unique=True)
+    assignment_name = db.Column(db.String(50),nullable=False)
+    assignment_code = db.Column(db.String(50),nullable=False)
+    assignment_subject_Name = db.Column(db.String(50),nullable=False)
     created_at = db.Column(db.DateTime,default=datetime.utcnow)
     updated_at = db.Column(db.DateTime,onupdate=datetime.utcnow)
     # fk
-    student_id = db.Column(db.Integer,db.ForeignKey('student.id'))
+    classroom_id = db.Column(db.Integer,db.ForeignKey('classroom.id'))
     # relationships
-    student = db.relationship("Student",backref="student_attendance",uselist=False,lazy="joined")
+    classroom = db.relationship("Classroom",backref="class_assignments",uselist=False,lazy="joined")
 
 
 
